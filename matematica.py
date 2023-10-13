cfe5919553f72215f6dd0a8d76f0fe87cb15eeaa
Giorgio Tentorio <giorgiotentorio@gmail.com>
2023-10-13 10:45:06 +0200

    implemented things

---
+++ matematica.py
@@ -0,0 +1,10 @@
+def operazioneStupida(a, e, m):
+    return (a**e) % m
+
+def operazionePocoIntelligente():
+    return 0
+
+def operazioneIntelligente():
+    return 0
+
+print(operazioneStupida(611, 9876, 10007))
